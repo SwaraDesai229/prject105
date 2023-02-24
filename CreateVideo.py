066faf89c32fb2aad86407fe8c476d40cab48b65
SwaraDesai229 <96266788+SwaraDesai229@users.noreply.github.com>
2023-02-23 19:22:45 -0500

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,10 @@
+from distutils import filelist
+import cv2
+import os
+path="Images/"
+image=[]
+os.listdir(path)
+os.splitext(file)
+if ext in ['.gif','.png',.'jpg','.jpeg','.jfif']:
+    file_name=path+"/"+file
+    
